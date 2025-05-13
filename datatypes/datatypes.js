236aae168249ea0615bc60fa6c8b74063fed5dfe
rupayan <rupbera922@gmail.com>
2025-05-13 09:48:10 +0530

    second day of practice variable and datatypes

---
+++ datatypes/datatypes.js
@@ -0,0 +1,27 @@
+// Types of Data Types
+
+// Premitive Data Types
+
+
+//Number 
+
+let number = 1;
+
+// Undefined 
+
+let a;
+console.log(a); // Out Put is Undefined
+
+//Null 
+let b=null;
+console.log(b);
+
+//Strings
+
+let u="Rupayan Bera";
+console.log(u);
+
+//Boolean 
+
+let i=true;
+console.log( typeof(i)); // Typeof is a utility that can use for check the datatypes 
